81f58a7b82926c2d7c0917987ea877d18cbd0c87
Mia <mariamasha12345@gmail.com>
2025-02-24 00:59:58 +0300

    clear up code

--- feature_videoplayer/src/main/java/com/vk/feature_videoplayer/presentaion/PlayerScreenViewModel.kt
+++ feature_videoplayer/src/main/java/com/vk/feature_videoplayer/presentaion/PlayerScreenViewModel.kt
@@ -1,9 +1,7 @@
 package com.vk.feature_videoplayer.presentaion
 
 import android.net.Uri
-import android.util.Log
 import androidx.core.net.toUri
-import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.viewModelScope
 import androidx.media3.common.MediaItem
 import com.vk.common.presentation.BaseViewModel
@@ -18,7 +16,6 @@ import javax.inject.Inject
 @HiltViewModel
 class PlayerScreenViewModel @Inject constructor(
     private val getSessionUseCase: GetSessionUseCase,
-    private val savedStateHandle: SavedStateHandle,
     private val audioServiceHandler: VideoServiceHandler,
 ): BaseViewModel<PlayerScreenContract.Event, PlayerScreenContract.State, PlayerScreenContract.Effect>() {
 
@@ -38,28 +35,9 @@ class PlayerScreenViewModel @Inject constructor(
         }
     }
 
-    fun addVideoUri(uri: Uri) {
-        savedStateHandle["videoUri"] = uri
-        audioServiceHandler.setMediaItem(MediaItem.fromUri(uri))
-//        player.addMediaItem(MediaItem.fromUri(uri))
-    }
-
     private fun playVideo(uri: Uri) {
         audioServiceHandler.setMediaItem(MediaItem.fromUri(uri))
-
-//        player.setMediaItem(MediaItem.fromUri(uri))
-//        player.setMediaItem(uri
-//            videoItems.value.find { it.contentUri == uri }?.mediaItem ?: return
-//        )
     }
 
-    override fun onCleared() {
-        super.onCleared()
-        //TODO: handle release
-//        audioServiceHandler.releasePlayer()
-    }
-
-    override fun handleEvent(event: PlayerScreenContract.Event) {
-        TODO("Not yet implemented")
-    }
+    override fun handleEvent(event: PlayerScreenContract.Event) {}
 }
